be26dd81ce5eb679546f6ddf9160e52a7e9582e4
kouheisatou <koheisato0420@gmail.com>
2022-10-20 18:16:58 +0900

    学籍番号が一致しないとキャンセルまたは利用終了できない

--- src/main/kotlin/components/dialogs/CardReaderDialog.kt
+++ src/main/kotlin/components/dialogs/CardReaderDialog.kt
@@ -1,6 +1,13 @@
 package components.dialogs
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.remember
 import common.RCS300
 import common.SHIBAURA_STUDENT_CARD_POLLING
 import common.SHIBAURA_STUDENT_CARD_SERVICE_CODE
@@ -37,21 +44,27 @@ class CardReaderDialog(
                     val studentId =
                         String(rcS300?.readBlock(SHIBAURA_STUDENT_CARD_SERVICE_CODE, 0) ?: continue).substring(3..9)
 
-                    // 学籍番号認証
-                    if (studentId.matches(Regex("[a-zA-Z]{2}[0-9]{5}")) && scanning) {
-                        println(studentId)
-                        val validateResult = onCardScanned?.invoke(studentId)
-                        if (validateResult == CardReaderResult.Succeeded) {
-                            scanning = false
-                            stopScan()
-                            showNextDialog(Selection.Positive)
-                        } else {
-                            println("学籍番号が異なります")
-                        }
-                    }
+                    validate(studentId)
                 } catch (e: RCS300.FelicaException) {
                     e.printStackTrace()
                 }
+            }
+        }
+    }
+
+    fun validate(studentId: String){
+
+        // 学籍番号認証
+        if (studentId.matches(Regex("[a-zA-Z]{2}[0-9]{5}")) && scanning) {
+            println(studentId)
+            val validateResult = onCardScanned?.invoke(studentId)
+            println(validateResult)
+            if (validateResult == CardReaderResult.Succeeded) {
+                scanning = false
+                stopScan()
+                showNextDialog(Selection.Positive)
+            } else {
+                println("学籍番号が異なります")
             }
         }
     }
@@ -81,6 +94,19 @@ fun CardReaderDialogComponent(cardReaderDialog: CardReaderDialog) {
         "学生証をカードリーダーにかざしてください",
         positiveButtonText = "",
         negativeButtonText = "",
+        additionalButtons = {
+            var studentIdTextBoxValue by remember { mutableStateOf("AF20000") }
+            Row {
+                OutlinedTextField(value = studentIdTextBoxValue, onValueChange = { text -> studentIdTextBoxValue = text })
+                Button(
+                    onClick = {
+                        cardReaderDialog.validate(studentIdTextBoxValue)
+                    }
+                ) {
+                    Text("学生証をかざした（仮）")
+                }
+            }
+        }
     )
 }
 
